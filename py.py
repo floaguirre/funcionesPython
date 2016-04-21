ab3aefa6ff4b41bd6d7297388e593e5544140afd
anita.pokorasky <anita.pokorasky@gmail.com>
2016-04-21 16:57:45 -0300

    ejercicio D

---
+++ py.py
@@ -0,0 +1,7 @@
+#d) Disenie una funcion que permita el ingreso de dos numeros naturales 
+#j y h e imprima jh utilizando solo la operacion multiplicacion."""
+
+j = input ("ingrese un numero natural: ") 
+h = input ("ingrese otro numero natural: ")
+nombredemifuncion (j,h)
+
